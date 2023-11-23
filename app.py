e41bf84333a9be8ed304859f5c55af54e9ee4f39
JezBeard <jezbeard@gmail.com>
2023-11-23 15:46:20 +0800

    Update app.py

--- app.py
+++ app.py
@@ -22,6 +22,9 @@ embeddings = OpenAIEmbeddings()
 
 embed_model = OpenAIEmbeddings(model="text-embedding-ada-002")
 
+# Create an instance of pinecone.Index
+index = pinecone.Index(index_name)
+
 index_name = "stockbroking"
 
 # Initialize the vector store object
