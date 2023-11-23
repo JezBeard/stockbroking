6a6ff1ce362741fd3a65c62865f01f184f01331f
JezBeard <jezbeard@gmail.com>
2023-11-24 07:30:51 +0800

    Update app.py

--- app.py
+++ app.py
@@ -27,7 +27,7 @@ embeddings = OpenAIEmbeddings()
 
 embed_model = OpenAIEmbeddings(model="text-embedding-ada-002")
 
-index_name = 'stock3'
+index_name = 'stock4'
 
 # Create an instance of pinecone.Index
 index = pinecone.Index(index_name)
