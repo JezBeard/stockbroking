5aa6df4d18f68e8cb54826d758320a78587d6e6d
JezBeard <jezbeard@gmail.com>
2023-11-30 09:36:45 +0800

    Update app.py

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ pinecone.environment = st.secrets["PINECONE_ENVIRONMENT"]
 
 embed_model = OpenAIEmbeddings(model="text-embedding-ada-002")
 
-index_name = 'stocks7'
+index_name = 'stocks9'
 
 # Create an instance of pinecone.Index
 index = pinecone.Index(index_name)
