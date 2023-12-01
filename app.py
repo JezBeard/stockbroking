b5f4543539ed379e059e454a7a0699f4405c18e1
JezBeard <jezbeard@gmail.com>
2023-12-01 10:53:38 +0800

    Update app.py

--- app.py
+++ app.py
@@ -42,7 +42,7 @@ def main():
     # Ask a question about the documents in the index
     query = st.text_input("Ask question's about ASX Quarterly Activites Reports :")
 
-    suggestions = ["", "Provide an overview of the Morella ASX announcement.", "What are the highlights of the Aurora Energy Quarterly Report?", "Provide a bullet point list of the key points mentioned in the Alpha HPA report", "Create the headings and subheadings for Powerpoint slides", "Translate the first paragraph to French"]
+    suggestions = ["", "Provide an overview of the Morella ASX announcement.", "What are the highlights of the Aurora Energy Quarterly Report?", "Provide a bullet point list of the key points mentioned in the Alpha HPA report"]
 
     suggestion = st.selectbox("Or select a suggestion: (ENSURE QUESTION FIELD ABOVE IS BLANK)", suggestions, index=0)
 
